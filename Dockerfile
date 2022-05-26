d4fbfdd74366561843b83444a347428b791bc50d
bernat <bernat.costa@cesurformacion.com>
2022-05-26 17:17:51 +0200

    cambios diseño

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM francarmona/docker-ubuntu16-apache2-php7-mssql_client
-COPY ./src /var/www/
+COPY ./src/index.php /var/www/
+COPY ./src/guardarintento.php /var/www/
 EXPOSE 80
--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 $a = session_id();
 if(empty($a)) session_start();
 $a = session_id();
-echo "<br />session_id(): " . $a
+//echo "<br />session_id(): " . $a
 ?>
 
 <?php
@@ -31,83 +31,108 @@ echo "<br />session_id(): " . $a
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wordle de Bernat</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" />
+
 	<style>
 
-td,th   {border:1px solid  green;}
-
 .A {
-	background-color:yellow;
+	background-color:#c9b458;
 }
 
 .G {
-	background-color:grey;
+	background-color:#787c7e;
 }
 
 .V {
-	background-color:green;
+	background-color:#6aaa64;
 }
-
+.wordle {
+  width:400px;
+ 
+}
 .cuadrado {
-	width:60px;
-	height:60px;
-	border:1px solid black;
-	margin: 10px;
-    float:left;
+	width:70px;
+	height:70px;
+	font-size:40px;
+	float:left;
+	margin: 5px;
+    border: 2px solid #787c7e;
+  	text-align: center;
+  	text-transform: uppercase;
+  	color:white;
+  	font: sans-serif;
+  	font-weight: bold;
 }
 .linea {
     width:100%;
-    float:left;
+	float:left;
+
+	
 }
 	</style>
 </head>
 <body>
-<h1>Wordle de Bernat</h1>
-<div class ="wordle">
-<?php 
+<div class="container">
+  <div class="abs-center">
+    
+ 
 
-$contador = 1;
+	<h1 >Wordle de Bernat</h1>
+	<div class ="wordle ">
+	<?php 
 
-while($row=sqlsrv_fetch_array($res))
-{
-		?>
+	$contador = 1;
+
+	while($row=sqlsrv_fetch_array($res))
+	{
+			?>
         <div class="linea">
-	<div class="cuadrado <?php echo $row['res1'];?>" > <?php echo $row['pal1'];?></div>
-	<div class="cuadrado <?php echo $row['res2'];?>" > <?php echo $row['pal2'];?></div>
-	<div class="cuadrado <?php echo $row['res3'];?>" > <?php echo $row['pal3'];?></div>
-	<div class="cuadrado <?php echo $row['res4'];?>" > <?php echo $row['pal4'];?></div>
-	<div class="cuadrado <?php echo $row['res5'];?>" > <?php echo $row['pal5'];?></div>
-
-    </div>
+			<div class="cuadrado <?php echo $row['res1'];?>" > <?php echo $row['pal1'];?></div>
+			<div class="cuadrado <?php echo $row['res2'];?>" > <?php echo $row['pal2'];?></div>
+			<div class="cuadrado <?php echo $row['res3'];?>" > <?php echo $row['pal3'];?></div>
+			<div class="cuadrado <?php echo $row['res4'];?>" > <?php echo $row['pal4'];?></div>
+			<div class="cuadrado <?php echo $row['res5'];?>" > <?php echo $row['pal5'];?></div>
+    	</div>
 	<?php
 	$contador++;
 }
 sqlsrv_close($con);
-while ($contador <6)
+$palabrasintentadas=$contador;
+while ($contador <=6)
 {
 	?>
-	<div class="linea">
-	<div class="cuadrado" ></div>
-	<div class="cuadrado" ></div>
-	<div class="cuadrado" ></div>
-	<div class="cuadrado" ></div>
-	<div class="cuadrado" ></div>
-</div>
+		<div class="linea">
+			<div class="cuadrado" ></div>
+			<div class="cuadrado" ></div>
+			<div class="cuadrado" ></div>
+			<div class="cuadrado" ></div>
+			<div class="cuadrado" ></div>
+		</div>
 	<?php
 	$contador++;
-
-
-
 }
 ?>
 
 
-
-</div>
-<form action="./guardarintento.php" method="POST">
-<input type="text" name="palabra" />
-<input type="submit" />
-</form>
+		<form  class="border p-3 form" method="post" action="./guardarintento.php">
+    	  	<div class="form-group">
+        		<input type="text" name="palabra" id="palabra" class="form-control" maxlength="5" autofocus <?php 
+		if($palabrasintentadas >6)
+			echo "disabled"
+		?>>
+      		</div>
+       	 	<button type="submit" class="btn btn-primary" style="display:none;" >enviar</button>
+    	</form>
 	
+
+	</div>
+
+  </div>
+</div>
 
 </body>
 </html>
